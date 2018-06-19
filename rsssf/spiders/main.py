d6163c16cdf79f88c2daf8f39a079d0c6381f949
Gabriel Kabbe <gabriel.kabbe@mail.de>
2018-06-19 23:37:00 +0200

    Update regex

--- rsssf/spiders/main.py
+++ rsssf/spiders/main.py
@@ -9,10 +9,15 @@ MONTHS = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split()
 
 def find_lists(text):
     result = re.findall(r"""\[
-                              (?:[\w\s]+\d+(?:\+\d+)?(?:pen)?[\,\;]?\s*)+
+                              (?:
+                                 ((?:[^\W\d]|\s)+)  # player name
+                                 (\d+(?:\+\d+)?)  # time
+                                 (?:pen)?[\,\;]?\s*
+                               )+
                             \]""", text, flags=re.VERBOSE)
-    return [r for r in result if not any(month in r.strip("[]").split()[0] for month in MONTHS)
-                                 and len(r) > 3]
+    return result
+    # return [r for r in result if not any(month in r.strip("[]").split()[0] for month in MONTHS)
+    #                              and len(r) > 3]
 
 
 class MainSpider(scrapy.Spider):
@@ -22,10 +27,13 @@ class MainSpider(scrapy.Spider):
 
     def parse(self, response):
         links = response.xpath("//a/@href").extract()
+        title = response.xpath("//title/text()").extract_first()
 
         lists = find_lists(response.text)
         if lists:
-            yield {"lists": lists}
+            yield {"title": title,
+                   "url": response.url,
+                   "lists": lists}
 
         for link in links:
             full_link = response.urljoin(link)
